a925784cc1d47c3329fd594731a6bf9a30ada126
Alfonso Yeste <68972892+alfonso-yeste@users.noreply.github.com>
2023-10-28 12:32:46 +0200

    Create variables1.js

---
+++ variables1.js
@@ -0,0 +1,14 @@
+//Declara una variable mutable
+let numero_de_personas = 2;
+
+//Declara una variable inmutable
+const const analytics_measurament_id = "G-123456789";
+
+//Imprimir en la consola
+console.info('Variables mutable: ' + numero_de_personas);
+console.info('Variables mutable: ' + analytics_measurament_id);
+
+//Intentar cambiar los valores de las variables
+numero_de_personas = 4;
+const analytics_measurament_id = "G-123";
+
