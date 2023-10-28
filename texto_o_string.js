53e3f12ec844e1bdacbc959f37ece93b19c6a5a7
Alfonso Yeste <68972892+alfonso-yeste@users.noreply.github.com>
2023-10-28 13:15:56 +0200

    Create texto_o_string.js

---
+++ texto_o_string.js
@@ -0,0 +1,8 @@
+// Definir una variable mutable y otra inmutable de texto
+let rol = "analista";
+const nombre = "Alfonso";
+const apellido = "Yeste";
+// Concatenar
+let autor_presentacion = rol + ", " + nombre;
+autor_presentacion += " " + apellido;
+console.info("Soy el " + autor_presentacion);
