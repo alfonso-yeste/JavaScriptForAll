5e88141f178181adee8fb8144a6f9dc922cfb8e1
Alfonso Yeste <68972892+alfonso-yeste@users.noreply.github.com>
2023-10-28 12:53:23 +0200

    Create operadores_logicos.js

---
+++ operadores_logicos.js
@@ -0,0 +1,13 @@
+//Soy la verdad y la mentira
+const verdadero = true;
+const falso = false;
+
+//Cierto y falso
+let verdad = 2 > 1;  //true
+let falso = 2 < 1;  //falso
+let iguales = 2 == 2;  //true
+let distintos = 2 == 1;  //false
+let casi_igual = 3 >= 1; //true
+let diferentes = 3 != 1; //true (son diferentes)
+let no_diferentes = 2 != 2; // (no son diferentes)
+
