45684a4ce061cfcb44875690ce1b4e5a3da32ec3
Alfonso Yeste <68972892+alfonso-yeste@users.noreply.github.com>
2023-10-28 12:45:56 +0200

    Create nombre_importa.js

---
+++ nombre_importa.js
@@ -0,0 +1,7 @@
+//Declara una varibale mutable
+let numero_de_personas = 6
+
+//Imprimir el resultado en consola
+console.info("Número de personas:" + Numero_de_personas);
+
+//como has podido comprobar te da error en la consola.
